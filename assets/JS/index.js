35c323ab4e4b052a1e3a376b4d626695858ec5c0
Iammachino11 <machino11.m11@gmail.com>
2024-11-27 09:00:59 +0100

    Added generate links functionality

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -6,6 +6,8 @@ const resetUsernameDiv = document.getElementById('reset-username');
 const resetUsernamePage = document.getElementById('reset-usernamePage');
 const resetPwdDiv = document.getElementById('reset-pwd');
 const resetPwdPage = document.getElementById('reset-pwd-page');
+const generateLinksDiv = document.getElementById('generate-links');
+const generateLinksPage = document.getElementById('generate-links-page')
 const backBtn = document.getElementById('back-btn');
 const themeToggle = document.querySelector('.switch input[type="checkbox"]');
 const body = document.body;
@@ -41,6 +43,7 @@ settingsMenu.addEventListener('click', stopEventPropagation);
 function displayResetUsernamePage() {
     resetUsernameDiv.style.display = 'none';
     resetPwdDiv.style.display = 'none';
+    generateLinksDiv.style.display= 'none';
     resetUsernamePage.style.display = 'block';
     closeBtn.style.display = 'none';
     backBtn.style.display = 'block';
@@ -55,6 +58,7 @@ function displayResetUsernamePage() {
   function displayResetPwdPage() {
     resetUsernameDiv.style.display = 'none';
     resetPwdDiv.style.display = 'none';
+    generateLinksDiv.style.display= 'none';
     resetPwdPage.style.display = 'block';
     closeBtn.style.display = 'none';
     backBtn.style.display = 'block';
@@ -64,13 +68,29 @@ function displayResetUsernamePage() {
   // Add event listener to reset password div
   resetPwdDiv.addEventListener('click', displayResetPwdPage);
   
+  // Function to display the generate links page
+  function displayGenerateLinksPage() {
+    resetUsernameDiv.style.display = 'none';
+    resetPwdDiv.style.display = 'none';
+    generateLinksDiv.style.display= 'none';
+    generateLinksPage.style.display = 'block';
+    closeBtn.style.display = 'none';
+    backBtn.style.display = 'block';
+    themeDiv.style.display = 'none';
+  }
+  
+  // Add event listener to generate links div
+  generateLinksDiv.addEventListener('click', displayGenerateLinksPage);
+
   // Function to go back to settings menu
   function goBackToSettingsMenu() {
     resetUsernameDiv.style.display = 'flex';
     resetPwdDiv.style.display = 'flex';
+    generateLinksDiv.style.display ='flex';
     themeDiv.style.display = 'flex';
     resetUsernamePage.style.display = 'none';
     resetPwdPage.style.display = 'none';
+    generateLinksPage.style.display ='none';
     closeBtn.style.display = 'block';
     backBtn.style.display = 'none';
   }
@@ -149,3 +169,24 @@ function setMinDateForToDateInput() {
 
 // Add event listener to from date input
 fromDateInput.addEventListener('input', setMinDateForToDateInput);
+
+
+const inputField = document.getElementById('generate-links-num');
+
+inputField.addEventListener('keypress', (e) => {
+  const inputValue = parseInt(inputField.value + e.key);
+  
+  if (isNaN(inputValue) || inputValue < 1 || inputValue > 100) {
+    e.preventDefault();
+  }
+});
+
+inputField.addEventListener('paste', (e) => {
+  e.preventDefault();
+  const pasteData = e.clipboardData.getData('Text');
+  const inputValue = parseInt(pasteData);
+  
+  if (!isNaN(inputValue) && inputValue >= 1 && inputValue <= 100) {
+    inputField.value = inputValue;
+  }
+});
